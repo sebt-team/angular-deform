6a14c11a91c13978a99f5cf62ca6ea6c761c759c
Cristian Orellana <cristianorellanabak@paddoo.com>
2016-07-07 18:06:47 -0300

    wip(): refactor builder controller

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,35 +5,51 @@ import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { NavbarDirective } from './components/navbar/navbar.directive';
 import { BuilderProvider } from './components/builder/builder.provider';
+import { fbFormObjectEditableController } from './components/builder/builder.controller';
 
 angular.module('angularDeforms', ['ngAnimate', 'ngTouch', 'ngMessages', 'ngAria', 'toastr'])
 
-	// -----------------------
-	// CONTANTS
-	// -----------------------
+  // -----------------------
+  // CONTANTS
+  // -----------------------
   .constant('moment', moment)
 
-	// -----------------------
-	// CONFIG
-	// -----------------------
+  // -----------------------
+  // CONFIG
+  // -----------------------
   .config(config)
 
-	// -----------------------
-	// RUN
-	// -----------------------
+  // -----------------------
+  // RUN
+  // -----------------------
   .run(runBlock)
 
-	// -----------------------
-	// CONTROLLERS
-	// -----------------------
+  // -----------------------
+  // CONTROLLERS
+  // -----------------------
   .controller('MainController', MainController)
 
-	// -----------------------
-	// DIRECTIVES
-	// -----------------------
+  // -----------------------
+  // DIRECTIVES
+  // -----------------------
   .directive('acmeNavbar', NavbarDirective)
 
-	// -----------------------
-	// PROVIDERS
-	// -----------------------
+  //  DIRECTIVE -----------------------------------------------------
+  import { BuilderProvider }                from './components/builder/builder.provider';
+  import { FbFormObjectEditableController } from './components/builder/builder.controller';
+  import { FbComponentsController }         from './components/builder/builder.controller';
+  import { fbFormObjectController }         from './components/builder/builder.controller';
+
+  // -----------------------
+  // PROVIDERS
+  // -----------------------
+  angular.module('builder.provider', [])
   .provider('$builder', BuilderProvider)
+
+  // -----------------------
+  // CONTROLLERS
+  // -----------------------
+  angular.module('builder.controller', ['builder.provider'])
+  .controller('fbFormObjectEditableController', ['$scope', '$injector', fbFormObjectEditableController])
+  .controller('fbComponentsController', ['$scope', '$injector', FbComponentsController])
+  .controller('fbFormObjectController', ['$scope', '$injector', fbFormObjectController])
