ded8f37e0e3e2b5cc44be58c279cdc7e84251181
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2016-09-09 09:03:40 -0300

    chore(): removes unused trash

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,9 +1,26 @@
-/* global moment:false */
 import { config } from './index.config';
-import { runBlock } from './index.run';
-import { MainController } from './main/main.controller';
-import { NavbarDirective } from './components/navbar/navbar.directive';
+import { BuilderProvider }                from './components/builder/builder.provider';
+import { DragProvider }                   from './components/builder/builder.drag';
+import { FbFormObjectEditableController } from './components/builder/builder.controller';
+import { FbBuilderController }            from './components/builder/builder.controller';
+import { FbComponentsController }         from './components/builder/builder.controller';
+import { FbComponentController }          from './components/builder/builder.controller';
+import { FbFormController }               from './components/builder/builder.controller';
+import { FbFormObjectController }         from './components/builder/builder.controller';
+import { DfDragpagesController }          from './components/builder/builder.controller';
+import { FbBuilder }                      from './components/builder/builder.directive';
+import { FbFormObjectEditable }           from './components/builder/builder.directive';
+import { FbObjectEditable }               from './components/builder/builder.directive';
+import { FbComponents }                   from './components/builder/builder.directive';
+import { FbComponent }                    from './components/builder/builder.directive';
+import { FbForm }                         from './components/builder/builder.directive';
+import { FbFormObject }                   from './components/builder/builder.directive';
+import { DfPageEditable }                 from './components/builder/builder.directive';
+import { DfDragpages }                    from './components/builder/builder.directive';
+import { Contenteditable }                from './components/builder/builder.directive';
+import { ComponentsBuilder }              from './components/builder/builder.components';
 
+// MAIN
 angular
   .module('angularDeforms',
     [
@@ -16,59 +33,24 @@ angular
       'validator.rules',
       'builderComponents'
     ])
-
-  // CONTANTS
-  .constant('moment', moment)
-
   // CONFIG
   .config(config)
 
-  // RUN
-  .run(runBlock)
-
-  // CONTROLLERS
-  .controller('MainController', MainController)
-
-  // DIRECTIVES
-  .directive('acmeNavbar', NavbarDirective);
-
-  //  DIRECTIVE -----------------------------------------------------
-  import { BuilderProvider }                from './components/builder/builder.provider';
-  import { DragProvider }                   from './components/builder/builder.drag';
-  import { FbFormObjectEditableController } from './components/builder/builder.controller';
-  import { FbBuilderController }            from './components/builder/builder.controller';
-  import { FbComponentsController }         from './components/builder/builder.controller';
-  import { FbComponentController }          from './components/builder/builder.controller';
-  import { FbFormController }               from './components/builder/builder.controller';
-  import { FbFormObjectController }         from './components/builder/builder.controller';
-  import { DfDragpagesController }          from './components/builder/builder.controller';
-  import { FbBuilder }                      from './components/builder/builder.directive';
-  import { FbFormObjectEditable }           from './components/builder/builder.directive';
-  import { FbObjectEditable }               from './components/builder/builder.directive';
-  import { FbComponents }                   from './components/builder/builder.directive';
-  import { FbComponent }                    from './components/builder/builder.directive';
-  import { FbForm }                         from './components/builder/builder.directive';
-  import { FbFormObject }                   from './components/builder/builder.directive';
-  import { DfPageEditable }                 from './components/builder/builder.directive';
-  import { DfDragpages }                    from './components/builder/builder.directive';
-  import { Contenteditable }                from './components/builder/builder.directive';
-  import { ComponentsBuilder }              from './components/builder/builder.components';
-
-  // MAIN
+// DIRECTIVE
 angular
   .module('builder', ['builderDirective']);
 
-  // PROVIDERS
+// PROVIDERS
 angular
   .module('builderProvider', [])
   .provider('$builder', BuilderProvider);
 
-  // CONFIG
+// CONFIG
 angular
   .module('builderComponents', ['builder', 'validator.rules'])
   .config(['$builderProvider', ComponentsBuilder]);
 
-  // CONTROLLERS
+// CONTROLLERS
 angular
   .module('builderController', ['builderProvider'])
   .controller('fbBuilderController', ['$scope', '$injector', FbBuilderController])
@@ -79,12 +61,12 @@ angular
   .controller('fbFormController', ['$scope', '$injector', FbFormController])
   .controller('dfDragpagesController', ['$scope', DfDragpagesController]);
 
-  // DRAG
+// DRAG
 angular
   .module('builderDrag', [])
   .provider('$drag', DragProvider);
 
-  // DIRECTIVES
+// DIRECTIVES
 angular
   .module('builderDirective', ['builderProvider', 'builderController', 'builderDrag', 'validator'])
   .directive('fbBuilder', ['$injector', FbBuilder])
